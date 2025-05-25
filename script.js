c85f67cef7f16c51b448b7299581f3666f13d60e
Isun ODON <115283479+IsunODON@users.noreply.github.com>
2025-05-25 12:15:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// text animation script
+var typed = new Typed(".typing", {
+    strings: ["Power Ranger Hitam :)"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    cursorChar: ' | ',
+    smartBackspace: true,
+    loop: true
+});
